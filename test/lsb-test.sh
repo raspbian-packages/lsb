890ac81fb735fccade48eea330cd86ea1c7c2aaa
Chris Lawrence <lawrencc@debian.org>
2008-08-04 20:29:54 -0500

    lsb 3.2-19 Debian release.

--- test/lsb-test.sh
+++ test/lsb-test.sh
@@ -4,5 +4,7 @@ echo "Importing $1/init-functions"
 . $1/init-functions
 
 log_warning_msg "Only a warning"
+log_success_msg "This should succeed"
+log_failure_msg "This fails miserably"
 
 echo "OK!"

7e9500b10baf10df9ed2f32ce1822ed738a982fb
Didier Raboud <odyx@debian.org>
2012-05-04 14:23:25 +0200

    PyUT: Add a unittest for lookup_codename.

--- test/test_lsb_release.py
+++ test/test_lsb_release.py
@@ -8,8 +8,16 @@ import string
 
 class TestLSBRelease(unittest.TestCase):
 
-	def test_void(self):
-		self.assertTrue('Void test')
+	def test_lookup_codename(self):
+		# Test all versions
+		for rno in lr.RELEASE_CODENAME_LOOKUP:
+			cdn = lr.RELEASE_CODENAME_LOOKUP[rno]
+			# Test that 1.1, 1.1r0 and 1.1.8 lead to buzz. Default is picked randomly and is not supposed to go trough
+			badDefault = ''.join( [random.choice(string.letters) for i in xrange(random.randint(0,9))])
+			self.assertEqual(lr.lookup_codename(rno,badDefault),cdn,'Release name `' + rno + '` is not recognized.')
+			self.assertEqual(lr.lookup_codename(rno + 'r' + str(random.randint(0,9)),badDefault),cdn,'Release name `' + rno + 'r*` is not recognized.')
+			self.assertEqual(lr.lookup_codename(rno + '.' + str(random.randint(0,9)),badDefault),cdn,'Release name `' + rno + '.*` is not recognized.')
+			self.assertEqual(lr.lookup_codename('inexistent_release' + str(random.randint(0,9)),badDefault),badDefault,'Default release codename is not accepted.')
 
 if __name__ == '__main__':
 	unittest.main()

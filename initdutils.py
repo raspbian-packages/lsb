8759b26d1532c16be72672da46b103d9950f9ff4
Jeff Licquia <jeff@virtpc-wheezy.internal.licquia.org>
2012-07-15 15:09:37 -0400

    Fix initdutils for Python 3 compatibility.

--- initdutils.py
+++ initdutils.py
@@ -1,13 +1,12 @@
 # Support for scanning init scripts for LSB info
 
-import re, sys, os, cStringIO
-import cPickle
+import re, sys, os
+import pickle
 
 try:
-    assert True
-except:
-    True = 1
-    False = 0
+    from io import StringIO
+except ImportError:
+    from cStringIO import StringIO
 
 class RFC822Parser(dict):
     "A dictionary-like object."
@@ -15,14 +14,14 @@ class RFC822Parser(dict):
     
     def __init__(self, fileob=None, strob=None, startcol=0, basedict=None):
         if not fileob and not strob:
-            raise ValueError, 'need a file or string'
+            raise ValueError('need a file or string')
         if not basedict:
             basedict = {}
         
         super(RFC822Parser, self).__init__(basedict)
 
         if not fileob:
-            fileob = cStringIO.StringIO(strob)
+            fileob = StringIO(strob)
 
         key = None
         for line in fileob:
@@ -119,7 +118,7 @@ def load_facilities():
                 scriptname, name, start, stop = line.strip().split()
                 facilities.setdefault(name, {})[scriptname] = (int(start),
                                                                int(stop))
-            except ValueError, x:
+            except ValueError as x:
                 print >> sys.stderr, 'Invalid facility line', line
 
     return facilities
@@ -174,4 +173,4 @@ def save_lsbinstall_info(filemap):
     fh.close()
 
 if __name__ == '__main__':
-    print scan_initfile('init-fragment')
+    print (scan_initfile('init-fragment'))

6684d327586112cc777ac89fc422bfde2724d2e6
Didier Raboud <odyx@debian.org>
2013-05-15 21:27:53 +0200

    Get Distributor ID from /etc/dpkg/origins/default if existant
    This helps Debian derivatives to avoid forking the lsb package.
    
    Closes: #703677

--- test/test_lsb_release.py
+++ test/test_lsb_release.py
@@ -217,6 +217,27 @@ class TestLSBRelease(unittest.TestCase):
 
 	def test_guess_debian_release(self):
 		distinfo = get_arch_distinfo()
+		
+		# Test different dpkg origin with an fake "unstable releases" that ends in /sid, and an invalid apt-cache policy
+		distinfo['ID'] = rnd_string(5,12)
+		fn = 'test/dpkg_origins_default_' + rnd_string(5,5)
+		f = open(fn,'w')
+		f.write('Vendor: ' + distinfo['ID'] + "\n")
+		f.close()
+		os.environ['LSB_ETC_DPKG_ORIGINS_DEFAULT'] = fn
+		
+		distinfo['RELEASE']  = 'testing/unstable'
+		distinfo['DESCRIPTION'] = '%(ID)s %(OS)s %(RELEASE)s' % distinfo
+		fn2 = 'test/debian_version_' + rnd_string(5,12)
+		f = open(fn2,'w')
+		f.write(rnd_string(5,12) + '/sid')
+		f.close()
+		os.environ['LSB_ETC_DEBIAN_VERSION'] = fn2
+		self.assertEqual(lr.guess_debian_release(),distinfo)
+		os.remove(fn)
+		# Make sure no existing /etc/dpkg/origins/default is used
+		os.environ['LSB_ETC_DPKG_ORIGINS_DEFAULT'] = '/non-existant'
+		distinfo['ID'] = 'Debian'
 
 		# Test "stable releases" with numeric debian_versions
 		for rno in lr.RELEASE_CODENAME_LOOKUP:
